d57dd7da60f28434bc175ec5ee4c3654f63a753d
tomasro27 <tomasrodriguez@utexas.edu>
2015-02-08 09:09:18 -0500

    added tab icons

--- app/src/main/java/hackatbrown/com/myopersonaltrainer/TabsPagerAdapter.java
+++ app/src/main/java/hackatbrown/com/myopersonaltrainer/TabsPagerAdapter.java
@@ -17,9 +17,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class TabsPagerAdapter extends FragmentPagerAdapter {
 
     private final int[] ICONS = new int[] {
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher,
-            R.drawable.ic_launcher
+            R.drawable.myo_tab,
+            R.drawable.dumbbell_tab,
+            R.drawable.music_tab
     };
 
     /**
